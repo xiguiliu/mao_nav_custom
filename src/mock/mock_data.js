83adeed18441ad856d5f0e09439b333c146c8423
xiguiliu <484777573@qq.com>
2026-06-06 14:00:04 +0800

    chore: 更新导航数据 - 2026/6/6 14:00:06

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -184,13 +184,6 @@ export const mockData = {
           "icon": "https://www.kancloud.cn/asset/app/images/logo.png"
         },
         {
-          "id": "site-1755077605625",
-          "name": "微信小程序",
-          "url": "https://developers.weixin.qq.com/miniprogram/dev/framework/quickstart/getstart.html",
-          "description": "",
-          "icon": "data:image/svg+xml;charset=UTF-8,%3csvg width='36' height='36' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M27.4362985 16.6454925c-.7072239 1.1607165-1.8835821 2.0505075-3.2927761 2.4601791-.0354627.0102687-.0705672.0212538-.1062687.0309254-.2176716.0618508-.4409552.0997015-.6622089.0997015-.9763582 0-1.5276418-.6594627-1.2315224-1.4730746.216597-.5949851.785194-1.097194 1.4791642-1.3296717 1.0755821-.3922388 1.8271044-1.2868059 1.8271044-2.3277612 0-1.4026268-1.3645373-2.5397014-3.0476417-2.5397014-1.6832239 0-3.0476418 1.1370746-3.0476418 2.5397014v7.788418c0 1.8730746-1.0852538 3.515582-2.709015 4.4445373-.8851343.506388-1.9277612.8040597-3.0475224.8040597-3.1743283 0-5.75665667-2.3545075-5.75665667-5.248597 0-.9211941.26340299-1.7863881.72238806-2.5397015.71032836-1.1658508 1.89397011-2.0586269 3.31164181-2.4656717.001194-.0003582.0022686-.0007164.0034627-.0010746.2390447-.0743881.4869253-.1157015.7313433-.1157015.9806567 0 1.5345671.6625672 1.2371343 1.4797612-.2015523.553791-.7443582 1.0306866-1.377194 1.2844179-.0755821.025194-.1491344.0534925-.2217314.0833433-1.0052537.4150448-1.6980298 1.2762985-1.6980298 2.2746269 0 1.4026268 1.3644179 2.539582 3.0476417 2.539582 1.6831045 0 3.0475224-1.1369552 3.0475224-2.539582v-7.788418c0-1.873194 1.0851344-3.515582 2.709015-4.44465667.8851343-.50638806 1.9277612-.8040597 3.0476418-.8040597 3.1742089 0 5.7566567 2.35450747 5.7566567 5.24871637 0 .9210747-.2635224 1.7863881-.7225075 2.5397015M18 2C9.16346269 2 2 9.16346269 2 18c0 8.8365373 7.16346269 16 16 16 8.8365373 0 16-7.1634627 16-16 0-8.83653731-7.1634627-16-16-16' fill='%2307C160' fill-rule='evenodd'/%3e%3c/svg%3e"
-        },
-        {
           "id": "site-1755074354606",
           "name": "Markdown",
           "url": "https://markdown.com.cn/",
@@ -220,13 +213,6 @@ export const mockData = {
           "icon": "/sitelogo/claude.ai.ico"
         },
         {
-          "id": "copilot",
-          "name": "GitHub Copilot",
-          "url": "https://github.com/features/copilot",
-          "description": "AI代码助手",
-          "icon": "/sitelogo/github.com.ico"
-        },
-        {
           "id": "cursor",
           "name": "Cursor",
           "url": "https://cursor.sh",
@@ -239,6 +225,27 @@ export const mockData = {
           "url": "https://www.doubao.com/chat/",
           "description": "",
           "icon": "https://gimg3.baidu.com/topone/src=https%3A%2F%2Fbkimg.cdn.bcebos.com%2Fsmart%2F7dd98d1001e93901213f5b9bfbb543e736d12f2e3846-bkimg-process%2Cv_1%2Crw_1%2Crh_1%2Cmaxl_800%2Cpad_1%3Fx-bce-process%3Dimage%2Fresize%2Cm_pad%2Cw_348%2Ch_348%2Ccolor_ffffff&refer=http%3A%2F%2Fwww.baidu.com&app=2011&size=f200,200&n=0&g=0n&er=404&q=75&fmt=auto&maxorilen2heic=2000000?sec=1755190800&t=2acdc8d7cd0b6f5647fde227b1d591ea"
+        },
+        {
+          "id": "site-1780725349620",
+          "name": "CPA",
+          "url": "http://127.0.0.1:8317/management.html#/login",
+          "description": "",
+          "icon": ""
+        },
+        {
+          "id": "site-1780725381874",
+          "name": "deepseek",
+          "url": "https://platform.deepseek.com",
+          "description": "",
+          "icon": "https://www.google.com/s2/favicons?domain=platform.deepseek.com&sz=128"
+        },
+        {
+          "id": "site-1780725419651",
+          "name": "硅基流动",
+          "url": "https://siliconflow.cn/",
+          "description": "",
+          "icon": "https://www.google.com/s2/favicons?domain=siliconflow.cn&sz=128"
         }
       ]
     },
@@ -268,13 +275,6 @@ export const mockData = {
           "url": "https://cloud.tencent.com",
           "description": "腾讯云计算服务",
           "icon": "/sitelogo/cloud.tencent.com.ico"
-        },
-        {
-          "id": "huawei-cloud",
-          "name": "华为云",
-          "url": "https://www.huaweicloud.com",
-          "description": "华为云计算服务",
-          "icon": "/sitelogo/www.huaweicloud.com.ico"
         }
       ]
     },
@@ -340,20 +340,6 @@ export const mockData = {
           "url": "https://www.52pojie.cn/",
           "description": "软件安全与破解技术论坛",
           "icon": "/sitelogo/www.52pojie.cn.ico"
-        },
-        {
-          "id": "site-1755075492731",
-          "name": "恩山论坛",
-          "url": "https://www.right.com.cn/forum/",
-          "description": "",
-          "icon": "https://cloudflare-imgbed-8h4.pages.dev/file/1755075484662_0e420b52b1094f4288fe3734de7e6298.png"
-        },
-        {
-          "id": "site-1755075410388",
-          "name": "远景论坛",
-          "url": "https://bbs.pcbeta.com/",
-          "description": "",
-          "icon": "https://cloudflare-imgbed-8h4.pages.dev/file/1755075380080_7a87db4a06bd2d45672064bcad7617b4.png"
         },
         {
           "id": "stackoverflow",
@@ -549,20 +535,6 @@ export const mockData = {
       "order": 10,
       "sites": [
         {
-          "id": "site-1755078522704",
-          "name": "抖音",
-          "url": "https://www.douyin.com/",
-          "description": "",
-          "icon": "https://img0.baidu.com/it/u=360921970,1201638078&fm=253&fmt=auto&app=138&f=PNG?w=500&h=500"
-        },
-        {
-          "id": "bilibili",
-          "name": "哔哩哔哩",
-          "url": "https://www.bilibili.com",
-          "description": "弹幕视频网站",
-          "icon": "/sitelogo/www.bilibili.com.ico"
-        },
-        {
           "id": "youtube",
           "name": "YouTube",
           "url": "https://www.youtube.com",
@@ -628,7 +600,5 @@ export const mockData = {
       ]
     }
   ],
-  "title": "猫猫导航",
-  "_fileSha": "ba42ea61b725204f0bc6f6be2916cdfb0a45836a",
-  "search": "google"
+  "title": "猫猫导航"
 }
